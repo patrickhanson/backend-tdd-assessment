e3ad651b8e6e470ce4dcff256da061a2a6f85db5
Patrick Hanson <patrickhanson08@gmail.com>
2018-12-05 15:09:48 -0500

    Finished test cases

--- echo.py
+++ echo.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
+import sys
+
 """An enhanced version of the 'echo' cmd line utility"""
 
 __author__ = "???"
@@ -10,12 +13,32 @@ import sys
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    # Create and define your parser engine here, but DON'T PARSE.
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.'
+    )
+    parser.add_argument('text', help='text to be manipulated')
+    parser.add_argument('-u', '--upper', action='store_true', help='convert text to uppercase')
+    parser.add_argument('-l', '--lower', action='store_true', help='convert text to lowercase')
+    parser.add_argument('-t', '--title', action='store_true', help='convert text to titlecase')
+
+    return parser
 
 
 def main(args):
     """Implementation of echo"""
-    pass
+    # Needs more work here
+    parser = create_parser()
+    echo_input = parser.parse_args(args)
+    result = echo_input.text
+    if echo_input.upper:
+        result = echo_input.text.upper()
+    if echo_input.lower:
+        result = echo_input.text.lower()
+    if echo_input.title:
+        result = echo_input.text.title()
+    
+    return result
 
 
 if __name__ == '__main__':
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -5,6 +5,50 @@ import unittest
 import echo
 
 # Your test case class goes here
+class TestEcho(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = echo.create_parser()
+    
+    def test_upper_short(self):
+        arg_list = ['-u', 'hello']
+        namespace = self.parser.parse_args(arg_list)
+        # Was the '-u' parsed and converted into a namespace attribute?
+        self.assertTrue(namespace.upper)
+        # Did the program transform our text to uppercase?
+        self.assertEquals(echo.main(arg_list), 'HELLO')
+
+    def test_upper_long(self):
+        arg_list = ['--upper', 'hello']
+        namespace = self.parser.parse_args(arg_list)
+        # Was the '-u' parsed and converted into a namespace attribute?
+        self.assertTrue(namespace.upper)
+        # Did the program transform our text to uppercase?
+        self.assertEquals(echo.main(arg_list), 'HELLO')
+
+    def test_lower_short(self):
+        arg_list = ['-l', 'HELLO']
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.lower)
+        self.assertEquals(echo.main(arg_list), 'hello')
+
+    def test_lower_long(self):
+        arg_list = ['--lower', 'HELLO']
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.lower)
+        self.assertEquals(echo.main(arg_list), 'hello')
+
+    def test_title_short(self):
+        arg_list = ['-t', 'hello']
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.title)
+        self.assertEquals(echo.main(arg_list), 'Hello')
+
+    def test_title_long(self):
+        arg_list = ['--title', 'hello']
+        namespace = self.parser.parse_args(arg_list)
+        self.assertTrue(namespace.title)
+        self.assertEquals(echo.main(arg_list), 'Hello')
 
 
 if __name__ == '__main__':
